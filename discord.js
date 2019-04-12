e2d6e2814ecf1d463a5fc7e70544e455dcc68c09
AgentTripleCCC <AgentTripleCCC@Gmail.com>
2019-04-12 15:47:07 -0700

    Still havent done anything, just learning

--- discord.js
+++ discord.js
@@ -2,18 +2,25 @@
 const Discord = require('discord.js');
  const client = new Discord.Client();
 const config = require("./config.json");
+const randomnum = require("./random.js")
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+    if (!msg.content.startsWith(config.prefix) || msg.author.bot) {return;}
 
     if (msg.content.startsWith(config.prefix + 'pray')) {
-         msg.reply('Your prayers have been heard... Gaybel is pleased!');
+         msg.reply('Your prayers have been heard... Lord Gaybel is pleased!');
     }
+    
+    if(msg.content.startsWith(config.prefix + "fortune")) {
+          msg.reply('The fortune trumpet is not function currently... we are working hard to make sure that your fortunes are told.')
+          getRandomInt(1, 100);
+     }
  });
+
 
 
 client.login(config.TOKEN);
