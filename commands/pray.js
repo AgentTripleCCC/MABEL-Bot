99deb35ebbc8bd62c43ceb03164376c145fc77a8
AgentTripleCCC <AgentTripleCCC@Gmail.com>
2020-07-15 14:01:52 -0700

    Added dynamic command features

---
+++ commands/pray.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'pray',
+	description: 'Pray to the Lord and Savior!',
+	execute(message, args) {
+		message.channel.send('Your prayers have been heard... Lord Gaybel is pleased!');
+	},
+};
--- main.js
+++ main.js
@@ -15,14 +15,26 @@ SQL user server, point system for praying
 can redeem for rewards (mystery skin, buy food, etc)
 
 */
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const Sequelize = require('sequelize');
+
 const config = require("./config.json");
 const random = require("./random.js");
 const constants = require("./constants.js");
-const Sequelize = require('sequelize');
+const { FILE } = require('dns');
 
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles)
+{
+     const command = require(`./commands/${file}`);
+     client.commands.set(command.name, command);
+}
 //Listener event for when the bot sends a ready event (ex: logging in)
 client.on('ready', () => {
      console.log(`Logged in as ${client.user.tag}!`);
@@ -31,20 +43,24 @@ client.on('ready', () => {
  //Listener Event which runs whenever a message with prefix '!' is received
 client.on('message', message => {
 
-     var msg = message.content.toLowerCase(); //Turns message to uppercase, to make commands not case sensitive
-     var sender = message.author; //Author of the message is set to the sender
-     
-     console.log(msg);
-     if (!msg.startsWith(config.prefix) || sender.bot) {return;}
+     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-     if (msg.startsWith(config.prefix + 'pray')) {
-          message.channel.send('Your prayers have been heard... Lord Gaybel is pleased!'); 
+     const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+     const commandName = args.shift().toLowerCase(); //Turns message to uppercase, to make commands not case sensitive
+     const command = client.commands.get(commandName);
+
+     if (!client.commands.has(commandName)) {
+          message.reply("Command does not exist, check your spelling silly goose.")
      }
-    
-     if(msg.startsWith(config.prefix + "fortune")) {
-           message.channel.send('The fortune trumpet is not function currently... we are working hard to make sure that your fortunes are told.');
-           message.channel.send(random.getRandomInt(constants.MIN, constants.MAX));
+      console.log(args);
+      console.log(command);
+     try {
+          command.execute(message, args);
+     } catch (error) {
+          console.error(error);
+          message.channel.send("error");
      }
+
  });
 
 
